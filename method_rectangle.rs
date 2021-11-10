5bad7585116983be70dc35b242846ccd6a84021a
옥지원 <77279316+JiWonOck@users.noreply.github.com>
2021-11-10 17:29:04 +0900

    Create method_rectangle.rs

---
+++ method_rectangle.rs
@@ -0,0 +1,21 @@
+//Rectangle 구조체 상에 area 메소드 정의하기
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle { length: 50, width: 30 };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}
