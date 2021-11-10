5af0c6b475d7c9d22764ca647c8f75dfb3ed9bbc
옥지원 <77279316+JiWonOck@users.noreply.github.com>
2021-11-10 17:25:43 +0900

    Create struct_rectangle.rs
    기본, 튜플, 구조체 등 비교

---
+++ struct_rectangle.rs
@@ -0,0 +1,61 @@
+//길이와 너비가 각각의 변수에 지정된 사각형의 넓이 계산하기
+fn main() {
+    let length1 = 50;
+    let width1 = 30;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(length1, width1)
+    );
+}
+
+fn area(length: u32, width: u32) -> u32 {
+    length * width
+}
+
+//튜플을 이용하여 사각형의 길이와 너비를 명시하기
+fn main() {
+    let rect1 = (50, 30);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(rect1)
+    );
+}
+
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+//Rectangle 구조체 정의하기
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle { length: 50, width: 30 };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+}
+
+//Debug 트레잇을 파생시키기 위한 어노테이션의 추가 및 디버그 포맷팅을 이용한 Rectangle 인스턴스의 출력
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle { length: 50, width: 30 };
+
+    println!("rect1 is {:?}", rect1);
+}
+
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.length * rectangle.width
+}
