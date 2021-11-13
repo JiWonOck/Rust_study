e1014dd9312028e5b524fa95b663170a640f5b3d
옥지원 <77279316+JiWonOck@users.noreply.github.com>
2021-11-14 00:03:17 +0900

    Create mod_path_use_glob.rs

---
+++ mod_path_use_glob.rs
@@ -0,0 +1,14 @@
+//이름공간 내의 모든 아이템을 가져오기
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+
+use TrafficLight::*;
+
+fn main() {
+    let red = Red;
+    let yellow = Yellow;
+    let green = Green;
+}
