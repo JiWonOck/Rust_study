386c150c69b6879a0139f55a479a5441c227f12b
옥지원 <77279316+JiWonOck@users.noreply.github.com>
2021-11-13 23:39:47 +0900

    Create networkmod_and_clientmod.rs

---
+++ networkmod_and_clientmod.rs
@@ -0,0 +1,18 @@
+//src/lib.rs 내에 나란히 정의된 network 모듈과 client 모듈
+#![allow(unused)]
+fn main() {
+	mod network {
+	    fn connect() {
+	    }
+	}
+
+	mod client {
+	    fn connect() {
+	    }
+	}
+}
+/*
+communicator
+ ├── network
+ └── client
+*/
